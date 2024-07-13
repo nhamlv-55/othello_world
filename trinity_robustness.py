e8f0f5d97b49aebf851680be83e1595d68bad60b
Ubuntu <levn@acml.izo5rsv1z0gujft2m2rlbhrpch.bx.internal.cloudapp.net>
2024-07-13 03:03:24 +0000

    generate head robust queries

--- trinity_robustness.py
+++ trinity_robustness.py
@@ -10,7 +10,7 @@ import logging
 from mingpt.utils import set_seed, get_OthelloGPT, get_probe, gen_dataset, build_marabou_net
 from maraboupy import MarabouCore, Marabou, MarabouUtils
 import random
-
+from enum import Enum
 set_seed(44)
 DEVICE = torch.device('cpu')
 
@@ -19,6 +19,8 @@ white 0, blank 1, black 2
 """
 PROBE = get_probe(device=DEVICE)
 GPT_MODEL: GPTforProbeIA = get_OthelloGPT(probe_layer=8, device=DEVICE)
+
+# GOOD_GAMES = gen_dataset(DEVICE, 'good_games.pkl')
 with open("good_games.pkl", "rb") as f:
     GOOD_GAMES = pickle.load(f)
 DUMMY_INPUT = GOOD_GAMES[0]['h'][0][-1] #h shoud be of the shape B * T * 512
@@ -30,31 +32,43 @@ MAX_TIME = 600  # in seconds
 M_OPTIONS: MarabouCore.Options = Marabou.createOptions(verbosity=0,
                                                        initialSplits=4,
                                                        timeoutInSeconds=MAX_TIME,
-                                                       snc=True,
+                                                       snc=False,
                                                        numWorkers=32,
                                                        )
 
+M_OPTIONS: MarabouCore.Options = Marabou.createOptions()
 
+class TrinityProperty(Enum):
+    PROBE_ROBUST = 1
+    HEAD_ROBUST = 2
+    PROBE_PROVE_HEAD = 3
+    HEAD_PROVE_PROBE = 4
 
-
-def verify_probe(eps: float):
+def gen_verify_queries(eps: float, n_games: int, n_targets: int, task: TrinityProperty ):
     #reuse the m_probe. only input and output bounds are changed
     m_probe = build_marabou_net(PROBE, DUMMY_INPUT)
 
-    input_vars = m_probe.inputVars[0]
-    output_vars = m_probe.outputVars[0][0]
+    m_head = build_marabou_net(GPT_MODEL.head, DUMMY_INPUT)
 
-    for g in GOOD_GAMES[:1]:
-        #get 4 random target tiles to flip
-        targets = random.sample(range(64), 4)
 
-        for target in targets:
-            h_input = g['h'][0][-1].detach().numpy() #h is of the shape B * T * 512. B should always be 1
-            logging.debug(f"h_input shape: {h_input.shape}")
-            logging.debug(f"Input vars: {m_probe.inputVars[0]}")
-            logging.debug(f"Output vars: {m_probe.outputVars[0]}")
-            
 
+    for g in tqdm(GOOD_GAMES[:n_games]):
+        input_vars = m_probe.inputVars[0]
+        output_vars = m_probe.outputVars[0][0]
+        #set output conditions
+        if task==TrinityProperty.PROBE_ROBUST:
+            """
+            generate the constraints for probe robustness
+            """
+            #get 4 random target tiles to flip
+            targets = random.sample(range(64), n_targets)
+
+
+            for target in targets:
+                h_input = g['h'][0][-1].detach().numpy() #h is of the shape B * T * 512. B should always be 1
+                logging.debug(f"h_input shape: {h_input.shape}")
+                logging.debug(f"Input vars: {m_probe.inputVars[0]}")
+                logging.debug(f"Output vars: {m_probe.outputVars[0]}")
 
             #set input perturbation
             assert h_input.shape == m_probe.inputVars[0].shape
@@ -63,7 +77,6 @@ def verify_probe(eps: float):
                 m_probe.setLowerBound(input_vars[i], h_input[i] - eps)
                 m_probe.setUpperBound(input_vars[i], h_input[i] + eps)
 
-            #set output conditions
             #Can we flip tile Target?
             print(m_probe.outputVars[0].shape)
             print(f"Current value for tile {target}: {output_vars[target]} ~ {len(g['true_board'])}")
@@ -83,10 +96,47 @@ def verify_probe(eps: float):
 
             query.addEquation(constraint.toCoreEquation())
 
-            query_name =  f"queries/probe_robust/game_{g['game_idx']}_target_{target}_eps_{eps}.txt"
+            query_name =  f"queries/probe_robust/group_{g['game_idx']%8}/game_{g['game_idx']}_target_{target}_eps_{eps}.txt"
             MarabouCore.saveQuery(query, query_name)
 
+        elif task==TrinityProperty.HEAD_ROBUST:
+            """
+            generate the constrainst for head robustness
+            """
+            input_vars = m_head.inputVars[0]
+            output_vars = m_head.outputVars[0]
+            print("output vars", output_vars)
+            true_prediction = np.array(g['true_output'])
+            true_pred_label = np.argmax(true_prediction)
+            print("true pred label", true_pred_label)
+            adv_labels = set(range(1, 61))
+            adv_labels.remove(true_pred_label)
+            adv_labels = sorted(adv_labels)
+            adv_labels = random.sample(adv_labels, n_targets)
+            print("adv_labels:", adv_labels)
+            h_input = g['h_after_lnf'][0][-1].detach().numpy() #h is of the shape B * T * 512. B should always be 1
 
+            #set input perturbation
+            assert h_input.shape == m_head.inputVars[0].shape
+
+            for i in range(len(h_input)): #should be 0->511
+                m_head.setLowerBound(input_vars[i], h_input[i] - eps)
+                m_head.setUpperBound(input_vars[i], h_input[i] + eps)
+
+            for adv_l in adv_labels:
+                #set output constraint. Can we make some random move to be the best move?
+                query_name = f"queries/head_robust/group_{g['game_idx']%8}/game_{g['game_idx']}_adv_labels_{adv_l}_eps_{eps}.txt"
+                query = m_head.getMarabouQuery()
+                
+                constraint = MarabouUtils.Equation(MarabouCore.Equation.GE)
+                constraint.setScalar(P_ZERO)
+                constraint.addAddend(-1, output_vars[true_pred_label])
+                constraint.addAddend(1, output_vars[adv_l])
+               
+                query.addEquation(constraint.toCoreEquation())
+
+                MarabouCore.saveQuery(query, query_name)
 
 if __name__=="__main__":
-    verify_probe(0.05)
+    # gen_verify_probe_queries(eps = 0.1, n_games= 100, n_targets=4)
+    gen_verify_queries(eps = 0.1, n_games=100, n_targets=4, task=TrinityProperty.HEAD_ROBUST)
