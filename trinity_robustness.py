709a63972063af388e6eaf028959984b32f5715e
Ubuntu <levn@acml.izo5rsv1z0gujft2m2rlbhrpch.bx.internal.cloudapp.net>
2024-07-15 18:18:16 +0000

    make sure that the targets are the same across tasks

--- trinity_robustness.py
+++ trinity_robustness.py
@@ -41,110 +41,90 @@ M_OPTIONS: MarabouCore.Options = Marabou.createOptions(verbosity=0,
 
 M_OPTIONS: MarabouCore.Options = Marabou.createOptions()
 
+
+
 class TrinityProperty(Enum):
     PROBE_ROBUST = 1
     HEAD_ROBUST = 2
     PROBE_PROVE_HEAD = 3
     HEAD_PROVE_PROBE = 4
 
-def gen_verify_queries(eps: float, n_games: int, n_targets: int, task: TrinityProperty ):
-    #reuse the m_probe. only input and output bounds are changed
-
-
-
+def gen_verify_queries(eps: float, n_games: int, targets: List[int], task: TrinityProperty ):
     for g in tqdm(GOOD_GAMES[:n_games]):
         #set output conditions
         if task==TrinityProperty.PROBE_ROBUST:
             """
             generate the constraints for probe robustness
             """
-            m_probe = build_marabou_net(PROBE, DUMMY_INPUT)
-            #get 4 random target tiles to flip
-            targets = random.sample(range(64), n_targets)
-
-
             for target in targets:
+                m_probe = build_marabou_net(PROBE, DUMMY_INPUT)
                 h_input = g['h'][0][-1].detach().numpy() #h is of the shape B * T * 512. B should always be 1
                 logging.debug(f"h_input shape: {h_input.shape}")
                 logging.debug(f"Input vars: {m_probe.inputVars[0]}")
                 logging.debug(f"Output vars: {m_probe.outputVars[0]}")
 
-            #set input perturbation
-            assert h_input.shape == m_probe.inputVars[0].shape
-            input_vars = m_probe.inputVars[0]
-            output_vars = m_probe.outputVars[0][0]
-            print(output_vars)
+                #set input perturbation
+                assert h_input.shape == m_probe.inputVars[0].shape
+                input_vars = m_probe.inputVars[0]
+                output_vars = m_probe.outputVars[0][0]
+                print(output_vars)
 
-            for i in range(len(h_input)): #should be 0->511
-                m_probe.setLowerBound(input_vars[i], h_input[i] - eps)
-                m_probe.setUpperBound(input_vars[i], h_input[i] + eps)
+                for i in range(len(h_input)): #should be 0->511
+                    m_probe.setLowerBound(input_vars[i], h_input[i] - eps)
+                    m_probe.setUpperBound(input_vars[i], h_input[i] + eps)
 
-            #Can we flip tile Target?
-            print(m_probe.outputVars[0].shape)
-            print(f"Current value for tile {target}: {output_vars[target]} ~ {len(g['true_board'])}")
-            print(f"True argmax of the target tile {g['true_board'][target]}")
+                #Can we flip tile Target?
+                print(m_probe.outputVars[0].shape)
+                print(f"Current value for tile {target}: {output_vars[target]} ~ {len(g['true_board'])}")
+                print(f"True argmax of the target tile {g['true_board'][target]}")
 
-            #Can we force target to be smaller than target + 1 % 3
-            true_argmax = int(g['true_board'][target])
-            target_idx = output_vars[target][true_argmax]
-            adv_idx = output_vars[target][(true_argmax+1)%3]
-            print(f"target_idx: {target_idx}")
-            constraint = MarabouUtils.Equation(MarabouCore.Equation.GE)
-            constraint.setScalar(P_ZERO)
-            constraint.addAddend(-1, target_idx)
-            constraint.addAddend(1, adv_idx)
+                #Can we force target to be smaller than target + 1 % 3
+                true_argmax = int(g['true_board'][target])
+                target_idx = output_vars[target][true_argmax]
+                adv_idx = output_vars[target][(true_argmax+1)%3]
+                print(f"target_idx: {target_idx}")
+                constraint = MarabouUtils.Equation(MarabouCore.Equation.GE)
+                constraint.setScalar(P_ZERO)
+                constraint.addAddend(-1, target_idx)
+                constraint.addAddend(1, adv_idx)
 
-            query = m_probe.getMarabouQuery()
+                query = m_probe.getMarabouQuery()
 
-            query.addEquation(constraint.toCoreEquation())
+                query.addEquation(constraint.toCoreEquation())
 
-            query_name =  f"queries/probe_robust/group_{g['game_idx']%8}/game_{g['game_idx']}_target_{target}_eps_{eps}.txt"
-            MarabouCore.saveQuery(query, query_name)
+                query_name =  f"queries/probe_robust/game_{g['game_idx']}_target_{target}_eps_{eps}.txt"
+                MarabouCore.saveQuery(query, query_name)
 
         elif task==TrinityProperty.HEAD_ROBUST:
             """
             generate the constrainst for head robustness
             """
-            m_head = build_marabou_net(GPT_MODEL.head, DUMMY_INPUT)
+            for target in targets:
 
-            input_vars = m_head.inputVars[0]
-            output_vars = m_head.outputVars[0]
-            print("output vars", output_vars)
-            true_prediction = np.array(g['true_output'])
-            true_prediction = true_prediction > 0
-            true_prediction = true_prediction.astype(int).tolist()
-            # true_prediction = true_prediction[1:30] + [-1, -1, -1, -1] + true_prediction[30:]
-            true_prediction = np.array(true_prediction[1:] + [-1, -1, -1, -1]).reshape(8,8)
-            print(true_prediction)
-            true_pred_label = np.argmax(true_prediction)
+                m_head = build_marabou_net(GPT_MODEL.head, DUMMY_INPUT)
 
-            print("true pred label", true_pred_label)
-            print("true valid moves", g['true_valid_moves'])
-            adv_labels = set(range(1, 61))
-            adv_labels.remove(true_pred_label)
-            adv_labels = sorted(adv_labels)
-            adv_labels = random.sample(adv_labels, n_targets)
-            print("adv_labels:", adv_labels)
-            h_input = g['h'][0][-1].detach().numpy() #h is of the shape B * T * 512. B should always be 1
+                input_vars = m_head.inputVars[0]
+                output_vars = m_head.outputVars[0]
 
-            #set input perturbation
-            assert h_input.shape == m_head.inputVars[0].shape
+                h_input = g['h'][0][-1].detach().numpy() #h is of the shape B * T * 512. B should always be 1
 
-            for i in range(len(h_input)): #should be 0->511
-                m_head.setLowerBound(input_vars[i], h_input[i] - eps)
-                m_head.setUpperBound(input_vars[i], h_input[i] + eps)
+                #set input perturbation
+                assert h_input.shape == m_head.inputVars[0].shape
 
-            for adv_l in adv_labels:
-                #set output constraint. Can we make some random move to be the best move?
-                query_name = f"queries/head_robust/group_{g['game_idx']%8}/game_{g['game_idx']}_adv_labels_{adv_l}_eps_{eps}.txt"
+                for i in range(len(h_input)): #should be 0->511
+                    m_head.setLowerBound(input_vars[i], h_input[i] - eps)
+                    m_head.setUpperBound(input_vars[i], h_input[i] + eps)
+
+                #set SAFETY condition
+                #can we make a legal move into illegal or vice versa
+                print(f"flipping target: {target}")
+                if g['true_output'][target] > 0:
+                    m_head.setUpperBound(output_vars[target], N_ZERO)
+                else:
+                    m_head.setLowerBound(output_vars[target], P_ZERO)
+
                 query = m_head.getMarabouQuery()
-                
-                constraint = MarabouUtils.Equation(MarabouCore.Equation.GE)
-                constraint.setScalar(P_ZERO)
-                constraint.addAddend(-1, output_vars[true_pred_label])
-                constraint.addAddend(1, output_vars[adv_l])
-               
-                query.addEquation(constraint.toCoreEquation())
+                query_name = f"queries/head_robust/game_{g['game_idx']}_target_{target}_eps_{eps}.txt"
 
                 MarabouCore.saveQuery(query, query_name)
 
@@ -153,9 +133,6 @@ def gen_verify_queries(eps: float, n_games: int, n_targets: int, task: TrinityPr
             We are using the head predicting legal move only.
             (positive value is legal move. negative value is illegal move)
             """
-            #get 4 random target tiles to flip
-            targets = random.sample(range(64), n_targets)
-
             #for each target
             for target in targets:
                 #create a new IPQ for each target
@@ -204,13 +181,11 @@ def gen_verify_queries(eps: float, n_games: int, n_targets: int, task: TrinityPr
 
                 #======DONE with constraints
                 # save query
-                query_name =  f"queries/head_prove_probe_robust/group_{g['game_idx']%8}/game_{g['game_idx']}_target_{target}_eps_{eps}.txt"
+                query_name =  f"queries/head_prove_probe_robust/game_{g['game_idx']}_target_{target}_eps_{eps}.txt"
                 MarabouCore.saveQuery(query, query_name)
 
         elif task==TrinityProperty.PROBE_PROVE_HEAD:
             #get 4 random target tiles to flip legal into illegal or vice versa
-            targets = random.sample(range(1, 61), n_targets)
-
             for target in targets:
                 m_trinity = build_marabou_net(TRINITY_MODEL, DUMMY_INPUT)
                 h_input = g['h'][0][-1]
@@ -263,12 +238,14 @@ def gen_verify_queries(eps: float, n_games: int, n_targets: int, task: TrinityPr
                     c2.addAddend(-1, right_idx)
                     query.addEquation(c2.toCoreEquation())
                 
-                query_name =  f"queries/probe_prove_head_robust/group_{g['game_idx']%8}/game_{g['game_idx']}_target_{target}_eps_{eps}.txt"
+                query_name =  f"queries/probe_prove_head_robust/game_{g['game_idx']}_target_{target}_eps_{eps}.txt"
                 MarabouCore.saveQuery(query, query_name)
                
 if __name__=="__main__":
+    #we sample only from 1 to 60 so we can use the same 4 targets for all 4 tasks (technically probe_robust should sample from 0 to 63)
+    TARGETS = random.sample(range(1, 61), 4) 
     # gen_verify_probe_queries(eps = 0.1, n_games= 100, n_targets=4)
-    gen_verify_queries(eps = 0.05, n_games=1, n_targets=4, task=TrinityProperty.HEAD_ROBUST)
-    gen_verify_queries(eps = 0.05, n_games=1, n_targets=4, task=TrinityProperty.PROBE_ROBUST)
-    gen_verify_queries(eps = 0.05, n_games=1, n_targets=4, task=TrinityProperty.HEAD_PROVE_PROBE)
-    gen_verify_queries(eps = 0.05, n_games=1, n_targets=4, task=TrinityProperty.PROBE_PROVE_HEAD)
+    gen_verify_queries(eps = 0.05, n_games=1, targets=TARGETS, task=TrinityProperty.HEAD_ROBUST)
+    gen_verify_queries(eps = 0.05, n_games=1, targets=TARGETS, task=TrinityProperty.PROBE_ROBUST)
+    gen_verify_queries(eps = 0.05, n_games=1, targets=TARGETS, task=TrinityProperty.HEAD_PROVE_PROBE)
+    gen_verify_queries(eps = 0.05, n_games=1, targets=TARGETS, task=TrinityProperty.PROBE_PROVE_HEAD)
